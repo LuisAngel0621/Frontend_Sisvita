16c042103f617e67fb3ca1e7430df8502d3f2103
Eduardo20232 <89710232+Eduardo5618@users.noreply.github.com>
2024-06-21 21:10:22 -0500

    V 0.3.4
    arreglo de la vista y añadido de la escala

--- app/src/main/java/com/example/proyecto_sisvita/registro/TestIsra/ISRA/InicioCognitivo.kt
+++ app/src/main/java/com/example/proyecto_sisvita/registro/TestIsra/ISRA/InicioCognitivo.kt
@@ -1,36 +1,28 @@
 package com.example.proyecto_sisvita.registro.TestIsra.ISRA
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.proyecto_sisvita.R
-import com.example.proyecto_sisvita.TestISRA
 import com.example.proyecto_sisvita.ui.theme.ProyectoSISVITATheme
 import com.example.proyecto_sisvita.viewmodel.PreguntasTestViewModel
 import com.example.proyecto_sisvita.viewmodel.RespuestasTestViewModel
@@ -65,10 +57,10 @@ fun MostrarPreguntasConRespuestas(
     listaPreguntas: List<ArrayList<String>>,
     listaRespuestas: List<ArrayList<String>>
 ) {
-    val selectedAnswers = remember { mutableStateOf(mutableListOf<Pair<Int, Int>>()) }
+    val selectedAnswers = remember { mutableStateListOf<Triple<Int, Int, Int>>()}
 
-    Column {
-        LazyColumn {
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.padding(bottom = 100.dp)) {
             itemsIndexed(listaPreguntas) { indexPregunta, pregunta ->
                 Card(
                     modifier = Modifier
@@ -82,18 +74,18 @@ fun MostrarPreguntasConRespuestas(
                         // Verificar que existan respuestas asociadas a esta pregunta
                         if (indexPregunta < listaPreguntas.size) {
                             val respuestasPregunta = listaRespuestas
-                            respuestasPregunta.forEachIndexed {indexRespuesta,respuesta ->
+                            respuestasPregunta.forEachIndexed { indexRespuesta, respuesta ->
                                 Row(
                                     verticalAlignment = Alignment.CenterVertically,
                                     modifier = Modifier.padding(start = 16.dp)
                                 ) {
                                     Checkbox(
-                                        checked = selectedAnswers.value.any { it.first == pregunta[1].toInt() && it.second == respuesta[1].toInt() },
+                                        checked = selectedAnswers.any { it.first == pregunta[1].toInt() && it.second == respuesta[1].toInt() },
                                         onCheckedChange = { isChecked ->
                                             if (isChecked) {
-                                                selectedAnswers.value.add(Pair(pregunta[1].toInt(), respuesta[1].toInt()))
+                                                selectedAnswers.add(Triple(pregunta[1].toInt(), respuesta[1].toInt(), 0))
                                             } else {
-                                                selectedAnswers.value.remove(Pair(pregunta[1].toInt(), respuesta[1].toInt()))
+                                                selectedAnswers.removeIf { it.first == pregunta[1].toInt() && it.second == respuesta[1].toInt() }
                                             }
                                         },
                                         colors = CheckboxDefaults.colors(
@@ -101,21 +93,73 @@ fun MostrarPreguntasConRespuestas(
                                             uncheckedColor = Color.Black
                                         )
                                     )
-                                    Text(text = " ${respuesta[1]} - ${respuesta[0]} ",
+                                    Text(
+                                        text = "${respuesta[0]} ",
                                         modifier = Modifier.clickable {
-                                            selectedAnswers.value.add(Pair(pregunta[1].toInt(), respuesta[1].toInt()))
+                                            if (!selectedAnswers.contains(Triple(pregunta[1].toInt(), respuesta[1].toInt(), 0))) {
+                                                selectedAnswers.add(Triple(pregunta[1].toInt(), respuesta[1].toInt(), 0))
+                                            }
                                         })
                                 }
                             }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            ScaleSelector(
+                                questionId = pregunta[1].toInt(),
+                                selectedAnswers = selectedAnswers
+                            )
                         }
                     }
                 }
             }
         }
-            Button(onClick = {
-                println("Selected answers: $selectedAnswers")
-            }) {
-                Text("Submit Answers")
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            onClick = {
+                println("Selected answers: ${selectedAnswers.toList()}")
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            Text("Enviar Respuestas")
+        }
+    }
+}
+
+@Composable
+fun ScaleSelector(
+    questionId: Int,
+    selectedAnswers: MutableList<Triple<Int, Int, Int>>
+) {
+    val initialScale = selectedAnswers.find { it.first == questionId }?.third ?: 0
+    val selectedScale = remember { mutableStateOf(initialScale) }
+
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(text = "Selecciona una escala de 1 a 5:")
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            for (i in 1..5) {
+                Text(text = "$i",
+                    modifier = Modifier.width(10.dp)
+                )
+                RadioButton(
+                    selected = (selectedScale.value == i),
+                    onClick = {
+                        selectedScale.value = i
+                        val currentAnswer = selectedAnswers.find { it.first == questionId }
+                        if (currentAnswer != null) {
+                            selectedAnswers[selectedAnswers.indexOf(currentAnswer)] = Triple(currentAnswer.first, currentAnswer.second, i)
+                        } else {
+                            selectedAnswers.add(Triple(questionId, 0, i))
+                        }
+                    },
+                    colors = RadioButtonDefaults.colors(
+                        selectedColor = Color.Green,
+                        unselectedColor = Color.Black
+                    )
+                )
             }
+            Spacer(modifier = Modifier.width(1.dp))
+        }
     }
 }
